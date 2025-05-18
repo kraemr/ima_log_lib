17049dbc9aecc7b2f4cf67d22697dc24a30a18f5
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2025-05-18 19:44:50 +0200

    changed templateName to uint8_t

--- inc/types.h
+++ inc/types.h
@@ -2,7 +2,7 @@
 
 #ifndef IMA_VERIFY_TYPES_H
     #define IMA_VERIFY_TYPES_H
-    #include <openssl/sha.h>
+#include <openssl/sha.h>
     #include <openssl/evp.h>
 #include <stdint.h>
     #define TEMPLATE_IMA_NG 0
@@ -22,7 +22,7 @@
         uint32_t pcrIndex;
         uint8_t hashOfTemplate[SHA256_DIGEST_LENGTH];
         uint32_t templateNameLength;
-        char templateName[16];
+        uint8_t templateName[16];
         uint32_t templateDataLength;
         uint8_t* templateData; // contains the hash we actually care about
     }ImaEventSha256;
@@ -31,7 +31,7 @@
         uint32_t pcrIndex;
         uint8_t hashOfTemplate[SHA_DIGEST_LENGTH];
         uint32_t templateNameLength;
-        char templateName[16];
+        uint8_t templateName[16];
         uint32_t templateDataLength;
         uint8_t* templateData; // contains the hash we actually care about
     }ImaEventSha1;
@@ -40,7 +40,7 @@
         uint32_t pcrIndex;
         uint8_t hashOfTemplate[SHA384_DIGEST_LENGTH];
         uint32_t templateNameLength;
-        char templateName[16];
+        uint8_t templateName[16];
         uint32_t templateDataLength;
         uint8_t* templateData; // contains the hash we actually care about
     }ImaEventSha384;
@@ -49,7 +49,7 @@
         uint32_t pcrIndex;
         uint8_t hashOfTemplate[SHA512_DIGEST_LENGTH];
         uint32_t templateNameLength;
-        char templateName[16];
+        uint8_t templateName[16];
         uint32_t templateDataLength;
         uint8_t* templateData; // contains the hash we actually care about
     }ImaEventSha512;
